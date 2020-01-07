0938fd42d767525c5eaf52588f23e531965c4c21
Alan Sun <alan.sun@student.isb.bj.edu.cn>
2020-01-07 09:23:23 +0800

    file upload system complete

--- app.py
+++ app.py
@@ -1,7 +1,41 @@
-from flask import render_template
+import os
+from flask import render_template, flash, request, redirect, url_for
+from werkzeug.utils import secure_filename
 from config import app
 
 
 @app.route('/')
 def index():
     return 'Hello world!'
+
+
+def allowed_files(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_image():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('No file part.')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_files(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('upload_image',
+                                        filename=filename))
+    else:
+        return '''
+                <!doctype html>
+                <title>Upload Image</title>
+                <h1>Upload Image</h1>
+                <form method=post enctype=multipart/form-data>
+                  <input type=file name=file>
+                  <input type=submit value=Upload>
+                </form>
+                '''
--- config.py
+++ config.py
@@ -2,5 +2,7 @@ from flask import Flask
 
 
 app = Flask(__name__)
+app.config['SESSION_TYPE'] = 'filesystem'
 app.config['UPLOAD_FOLDER'] = 'uploads/'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg', 'png'])
