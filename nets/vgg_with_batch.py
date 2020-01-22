e2acf32c6f85678ae8552b9eb1bc6e1af408ba01
alansun17904 <asun17904@gmail.com>
2020-01-22 21:27:09 +0800

    frozen all layers from vgg except for first layer

--- nets/vgg_with_batch.py
+++ nets/vgg_with_batch.py
@@ -4,21 +4,18 @@ from torchvision import models
 
 model = models.vgg16_bn(pretrained=True)
 model.classifier[6] = nn.Sequential(
-                      nn.Linear(512 * 7 * 7, 4096, bias=True),
+                      nn.Linear(512 * 7 * 7, 256, bias=True),
                       nn.BatchNorm1d(num_features=4096),
                       nn.ReLU(),
                       nn.Dropout(0.5),
-                      nn.Linear(4096, 265),
-                      nn.BatchNorm1d(num_features=265),
-                      nn.ReLU(),
-                      nn.Dropout(0.5),
-                      nn.Linear(256, 5),
+                      nn.Linear(256, 6),
                       nn.Softmax(dim=0))
 
 
-# count = 0
-# for param in model.parameters():
-#     count += 1
-
-# total_params = sum(p.numel() for p in model.parameters())
-# print(total_params)
+count = 0
+vgg = next(model.children())
+for param in vgg:
+    if count <= 39:
+        print(param)
+        param.requires_grad = False
+    count += 1
