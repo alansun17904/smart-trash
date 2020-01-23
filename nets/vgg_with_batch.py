5fc111e410eb754b2eff5a26f6a84ea99f2d59df
alansun17904 <asun17904@gmail.com>
2020-01-23 20:23:45 +0800

    modified  weight storage after training

--- nets/vgg_with_batch.py
+++ nets/vgg_with_batch.py
@@ -12,7 +12,7 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
 
-data_dir = 'nets/split-garbage-dataset'
+data_dir = 'data/split-garbage-dataset'
 
 # Data Augmentation and Normalization for Training
 data_transforms = {
@@ -48,21 +48,7 @@ dataset_sizes = {x: len(image_datasets[x]) for x in ['train', 'test', 'valid']}
 class_names = image_datasets['train'].classes
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-# def imshow(inp, title=None):
-#     inp = inp.numpy().transpose((1, 2, 0))
-#     mean = np.array([0.485, 0.456, 0.406])
-#     std = np.array([0.229, 0.224, 0.225])
-#     inp = std * inp + mean
-#     inp = np.clip(inp, 0, 1)
-#     plt.imshow(inp)
-#     if title is not None:
-#         plt.title(title)
-#     plt.pause(10)  # pause a bit so that plots are updated
-#
-# inputs, classes = next(iter(dataloaders['train']))
-# out = torchvision.utils.make_grid(inputs)
-# imshow(out, title=[class_names[x] for x in classes])
+print(dataset_sizes)
 
 model = models.vgg16_bn(pretrained=True)
 model.classifier[6] = nn.Sequential(
@@ -74,22 +60,28 @@ model.classifier[6] = nn.Sequential(
                       nn.Softmax(dim=0))
 
 
-count = 0
+c = 0
 vgg = next(model.children())
 for param in vgg:
-    if count <= 39:
+    if c <= 39:
+        if hasattr(param, 'weight') and hasattr(param, 'bias'):
+            param.weight.requires_grad = False
+            param.bias.requires_grad = False
         param.requires_grad = False
-    count += 1
+
+    c += 1
 
 
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), eps=0.5)
+optimizer = optim.Adam(filter(lambda p: p.requires_grad, model.parameters()), eps=0.5)
 
 since = time.time()
 best_acc = 0.
 best_model_wts = copy.deepcopy(model.state_dict())
 
-for epoch in range(15):
+print(model.state_dict())
+
+for epoch in range(200):
     print(f'Epoch: {epoch + 1}\n{"-" * 10}')
     for phase in ['train', 'valid']:
         if phase == 'train':
@@ -101,8 +93,9 @@ for epoch in range(15):
         correct = 0
 
         for inputs, labels in dataloaders[phase]:
+            # training on gpu
             inputs = inputs.to(device)
-            lables = labels.to(device)
+            labels = labels.to(device)
 
             # zero the optimizer
             optimizer.zero_grad()
@@ -120,14 +113,13 @@ for epoch in range(15):
             # add to stats
             run_loss += loss.item() * inputs.size(0)
             correct += torch.sum(preds == labels.data)
-            print(correct)
 
         epoch_loss = run_loss / dataset_sizes[phase]
         epoch_acc = correct.double() / dataset_sizes[phase]
         print('{} Loss: {:.4f}, Acc: {:.4f}'.format(phase.title(),
               epoch_loss, epoch_acc))
 
-        if phase == 'val' and epoch_acc > best_acc:
+        if phase == 'valid' and epoch_acc > best_acc:
             best_acc = epoch_acc
             best_model_wts = copy.deepcopy(model.state_dict())
 
@@ -135,3 +127,4 @@ time_elapsed = time.time() - since
 print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60,
                                                     time_elapsed % 60))
 print('Best val acc: {:4f}'.format(best_acc))
+torch.save(best_model_wts, 'nets/config/recycle_vgg.pth')
