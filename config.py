ac2bc23230d869c52a59db01f3b3eec3569654d3
alansun17904 <asun17904@gmail.com>
2020-08-20 11:19:47 -0400

    reorgnaized file structure for better integration with availink platform

--- config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-
-app = Flask(__name__)
-app.config['SESSION_TYPE'] = 'filesystem'
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-app.config['ALLOWED_EXTENSIONS'] = set(['jpg', 'jpeg', 'png'])
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
